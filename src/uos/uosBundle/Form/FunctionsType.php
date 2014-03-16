eb6288c6888c9c85aab1a7e69786ff3b5c7de6a8
Brunthavan Murugaiah <bruntha10@gmail.com>
2014-03-16 23:12:28 +0530

    functions 2

---
+++ src/uos/uosBundle/Form/FunctionsType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace uos\uosBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class HallType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('hallname')
+            ->add('capacity')
+            ->add('gender','choice', array(
+    'choices' => array('Male' => 'Male', 'Female' => 'Female')));
+        
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'uos\uosBundle\Entity\Hall'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'uos_uosbundle_functions';
+    }
+}
